c9649d50d33a7ba1210f293de3cca98c553543ff
Bharadwaj <bharadwajkvsm@gmail.com>
2023-12-31 14:54:56 +0530

    again added cors

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const AutoIncrement = require("mongoose-sequence")(mongoose);
-// const autoIncrement = require("mongoose-auto-increment");
 
 const app = express();
-// app.UseCors((x) =>
-//   x
-//     .AllowAnyMethod()
-//     .AllowAnyHeader()
-//     .SetIsOriginAllowed((origin) => true) // allow any origin
-//     .AllowCredentials()
-// );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(cors());
 
 app.use(bodyParser.json());
 const Schema = mongoose.Schema;
